7644f644d88e4fc6ebc5903a057f1f769a2368c5
WallissonAlv <wallissonlima008@gmail.com>
2024-05-14 17:41:36 -0300

    Finalizando por hoje

--- 05 - POO/POO/src/ComputadorPedrinho.java
+++ 05 - POO/POO/src/ComputadorPedrinho.java
@@ -5,9 +5,15 @@ public class ComputadorPedrinho {
 	public static void main(String[] args) {
 		//abrindo MSN Messenger
 		MSNMessenger msn = new MSNMessenger();
-		
 		msn.enviarMensagem();
-		
 		msn.receberMensagem();
+		
+		FacebookMessenger face = new FacebookMessenger();
+		face.enviarMensagem();
+		face.receberMensagem();
+		
+		Telegram tel = new Telegram();
+		tel.enviarMensagem();
+		tel.receberMensagem();
 	}
 }
