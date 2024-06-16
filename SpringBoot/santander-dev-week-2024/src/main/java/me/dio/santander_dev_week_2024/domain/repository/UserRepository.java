7362d03a18b731059d6d146717007059ae3eae2d
Wallisson Alves <wallissonlima008@gmail.com>
2024-06-16 19:33:34 -0300

    Adicionando algumas funcionalidades

--- SpringBoot/santander-dev-week-2024/src/main/java/me/dio/santander_dev_week_2024/domain/repository/UserRepository.java
+++ SpringBoot/santander-dev-week-2024/src/main/java/me/dio/santander_dev_week_2024/domain/repository/UserRepository.java
@@ -8,4 +8,5 @@ import me.dio.santander_dev_week_2024.domain.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	boolean existsByAccountNumber(String accountNumber);
 }
